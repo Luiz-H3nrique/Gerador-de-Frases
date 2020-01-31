e136b0577f7e1d25d2f5b15bf4f61096872a1118
Luiz-H3nrique <luizsantos894@gmail.com>
2020-01-31 10:39:41 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Frases do Dia'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _FrasesAleatoria = 0;
+  List _frases = [
+    'o importante não  é vencer todos os dias, mas lutar sempre.',
+    'É melhor conquistar a si mesmo do que vencer mil batalhas.',
+    'o medo de perder tira a vontade de ganhar.',
+    'Perder para a razão, sempre é ganhar.'
+  ];
+
+
+
+  void _incrementCounter() {
+    setState(() {
+
+      _FrasesAleatoria = new Random().nextInt(3);
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pressione o botão para gerar uma frase :',
+            ),
+            Text(
+              _frases[_FrasesAleatoria],
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
